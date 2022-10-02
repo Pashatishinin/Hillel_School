f649f939e6c61bbbc1c6016bb09cc6154b099c86
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 12:50:45 +0200

    2. Дано целое число (int). Определить сколько нулей в конце этого числа.

---
+++ DAY7/Lesson7.2.py
@@ -0,0 +1,12 @@
+from random import randint
+
+a = randint(1,10000000000)
+b = str(a)[-1::-1]
+result = ""
+for i in b:
+    if i == "0":
+        result += i
+    elif i != "0":
+        break
+print("_______________")
+print(f"В конце числа {a} - {len(result)} нуля(ей)")
