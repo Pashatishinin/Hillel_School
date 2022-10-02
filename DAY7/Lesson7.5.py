69601869229fdbd5303428d41559ea6c261cb846
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 12:51:57 +0200

    5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.[1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)

---
+++ DAY7/Lesson7.5.py
@@ -0,0 +1,11 @@
+from random import randint
+
+my_list = [randint(1,100) for i in range(10)]
+
+print(f"""Изначальный список: 
+{my_list}""")
+
+my_list.append(my_list.pop(0))
+
+print("_______________")
+print(my_list)
