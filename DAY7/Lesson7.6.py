e2ba45c7576c19f04cac1f77aaad206b9a9c1bac
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 13:06:17 +0200

    6. Дана строка в которой есть числа (разделяются пробелами).Например "43 больше чем 34 но меньше чем 56". Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке.

---
+++ DAY7/Lesson7.6.py
@@ -0,0 +1,9 @@
+my_list = input("Введите элементы строки: ").split()
+total = 0
+for i in my_list:
+    if i.isdigit():
+        total += int(i)
+    else:
+        continue
+print("_______________")
+print(f"Сумма всех числе в этой строке равна - {total}")
