6c0e6c5678d3c411cfaba34e9534424d9ab64698
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 12:53:06 +0200

    3. Даны списки my_list_1 и my_list_2.Создать список my_result в который вначале поместитьэлементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.

--- DAY7/Lesson7.3.py
+++ DAY7/Lesson7.3.py
@@ -1,11 +1,10 @@
 from random import randint
 
-my_list_1 = [randint(1,100) for i in range(10)]
-my_list_2 = [randint(1,100) for i in range(10)]
+my_list_1 = [randint(1, 100) for i in range(10)]
+my_list_2 = [randint(1, 100) for i in range(10)]
 print(f"""Изначальные списки: 
 {my_list_1}
 {my_list_2}""")
-
 
 my_result = my_list_1[1::2] + my_list_2[0::2]
 print("_______________")
