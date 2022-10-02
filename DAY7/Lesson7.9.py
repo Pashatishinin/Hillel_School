3b1df65de215113f7f118ab4a9d92f08ba27ea45
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 13:43:19 +0200

    9. Дан список чисел. Определите, сколько в этом списке элементов,которые больше суммы двух своих соседей (слева и справа), и НАПЕЧАТАЙТЕ КОЛИЧЕСТВО таких элементов.Крайние элементы списка никогда не учитываются, поскольку у них недостаточно соседей.

--- DAY7/Lesson7.9.py
+++ DAY7/Lesson7.9.py
@@ -1,6 +1,6 @@
 from random import randint
 
-my_list = [randint(1,10)for i in range(10)]
+my_list = [randint(1, 10) for i in range(10)]
 print(f"""Изначальный список: 
 {my_list}""")
 
@@ -9,7 +9,7 @@ for j in my_list:
     k = my_list.index(j)
     if my_list[0] is j or my_list[-1] is j:
         pass
-    elif j > (my_list[k-1]+my_list[k+1]):
+    elif j > (my_list[k - 1] + my_list[k + 1]):
         result.append(j)
 
 print("_______________")
