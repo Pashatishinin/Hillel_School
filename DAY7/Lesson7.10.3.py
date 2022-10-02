edeec21a580ab44f932eb840f20cb642e98df339
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-02 18:18:01 +0200

    10. *Если использовать только одну переменную списка

--- DAY7/Lesson7.10.3.py
+++ DAY7/Lesson7.10.3.py
@@ -1,11 +1,9 @@
 my_list = list(input("Введите элементы строки: ").split())
 
-print(f"""Изначальный список:
-{my_list}""")
-
 for i in my_list:
     if i.isdigit():
         my_list.pop(my_list.index(i))
 
 print("_______________")
 print(my_list)
+
