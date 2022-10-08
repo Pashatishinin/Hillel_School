becdc2eb9a382fda02780a522390916037046f9b
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2022-10-08 21:01:07 +0200

    Главный модуль проверки работоспособности

---
+++ DAY9/Lesson9.main.py
@@ -0,0 +1,33 @@
+import Lesson9_1
+import Lesson9_2
+import Lesson9_3
+import Lesson9_4
+import Lesson9_5
+import Lesson9_6
+import Lesson9_7
+import Lesson9_8
+
+print("_______________")
+print(f"1) {Lesson9_1.list_reverced_start()}")
+
+print("_______________")
+print(f"2) {Lesson9_2.a_begginer_start()}")
+
+print("_______________")
+print(f"3) {Lesson9_3.a_searching_start()}")
+
+print("_______________")
+print(f"4) {Lesson9_4.list_wathcer_start()}")
+
+print("_______________")
+print(f"5) {Lesson9_5.one_count_symbol_start()}")
+
+print("_______________")
+print(f"6) {Lesson9_6.string_count_symbol_start()}")
+
+print("_______________")
+print(f"7) {Lesson9_7.string_one_symbol_start()}")
+
+print("_______________")
+print(f"8) {Lesson9_8.random_name_mail_start()}")
+
